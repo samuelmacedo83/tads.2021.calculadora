5c25e32d5fc38d207c5a3415b4081c1ba9f9cfac
samuelmacedo83 <svm.macedo@gmail.com>
2021-11-05 21:29:13 -0300

    first commit

---
+++ R/diminui.R
@@ -0,0 +1,15 @@
+#' Função diminui
+#'
+#' Esta é uma função simples para diminuir dois números
+#'
+#' @param a
+#' @param b
+#'
+#' @examples
+#' diminui(3,2)
+#' diminui(5,3)
+#'
+#' @export
+diminui <- function(a,b) {
+  a - b
+}
---
+++ R/soma.R
@@ -0,0 +1,16 @@
+#' Função soma
+#'
+#' Esta função é simples e soma dois números
+#'
+#' @param a Primeiro número
+#' @param b Segundo número
+#'
+#' @examples
+#' soma(2,3)
+#' soma(4,5)
+#'
+#' @export
+soma <- function(a, b) {
+  a + b
+}
+
